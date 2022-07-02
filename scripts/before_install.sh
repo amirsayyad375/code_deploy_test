f97d23ed82fb819870117552aca1d9595f9db63b
amirsayyad375 <78289173+amirsayyad375@users.noreply.github.com>
2022-07-02 21:02:09 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # I want to make sure that the directory is clean and has nothing left over from
 # previous deployments. The servers auto scale so the directory may or may not
